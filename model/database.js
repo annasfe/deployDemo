91daf2fedb2db847e41a2cca905af7910f4f6958
annasfe <anna_sfe@hotmail.com>
2023-10-11 11:16:57 +0300

    updated mysql to mysql2

--- model/database.js
+++ model/database.js
@@ -6,13 +6,14 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
 
 const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
   database: DB_NAME,
-  port: process.env.DB_PORT || "3306",
+  port: DB_PORT || "3306",
   multipleStatements: true
 });
 
